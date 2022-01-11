4a49a98f43031357d220fd5f455bffbc924bef29
hakeemanimashaun <82869130+hakeemanimashaun@users.noreply.github.com>
2022-01-11 06:18:39 +0100

    first commit

---
+++ app/src/main/java/com/assignment/myapplication/model/Presenter.kt
@@ -0,0 +1,37 @@
+package com.assignment.myapplication.model
+
+import com.assignment.myapplication.Contract
+
+
+// instantiating the objects of View and Model Interface
+// creating object of View Interface
+// creating object of Model Interface
+class Presenter (
+    private var mainView: Contract.View?,
+    private val model: Contract.Model) : Contract.Presenter,
+    Contract.Model.OnFinishedListener {
+
+        // operations to be performed
+        // on button click
+        override fun onButtonClick() {
+            if (mainView != null) {
+                mainView!!.showProgress()
+            }
+            model.getNextCourse(this)
+        }
+
+        override fun onDestroy() {
+            mainView = null
+        }
+
+        // method to return the string
+        // which will be displayed in the
+        // Course Detail TextView
+        override fun onFinished(string: String?) {
+            if (mainView != null) {
+                mainView!!.setString(string)
+                mainView!!.hideProgress()
+            }
+        }
+
+}
